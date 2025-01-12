b02536c0aa6d576669c7e0de427ec9e6d64e6bbb
Miguel <miguel_anxel@hotmail.com>
2025-01-12 17:45:23 -0500

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HeaderComponent } from '../components/header/header.component';
+import {MatIcon} from '@angular/material/icon';
+import { HomeComponent } from '../components/home/home.component';
+import { CardComponent } from '../components/card/card.component';
+import { ProductComponent } from '../components/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    CardComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIcon
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
